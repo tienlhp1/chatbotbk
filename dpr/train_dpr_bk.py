b05399022ba6077422630d479356d89305c9986e
tien nguyen <nguyenvantiendt09bkhn@gmail.com>
2024-01-24 02:01:56 +0700

    add train bkchatbot

--- dpr/train_dpr_bk.py
+++ dpr/train_dpr_bk.py
@@ -175,12 +175,12 @@ def main():
     torch.cuda.empty_cache()
     print("Check with the final state:")
     dpr_retriever = DPRRetriever(args, q_encoder, ctx_encoder, save_type="final")
-    dpr_retriever.test_on_data(top_k=[1, 5, 10, 30, 100])
+    dpr_retriever.test_on_data(top_k=[1, 5, 10, 30, 100], segmented=False)
     # dpr_retriever.increase_neg(no_negs=7, segmented=True)
     print("Check with the best state:")
     torch.cuda.empty_cache()
     dpr_retriever = DPRRetriever(args, save_type="best")
-    dpr_retriever.test_on_data(top_k=[1, 5, 10, 30, 100])
+    dpr_retriever.test_on_data(top_k=[1, 5, 10, 30, 100], segmented=False)
     # dpr_retriever.increase_neg(no_negs=7, segmented=True)
 
 
